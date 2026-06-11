dd42e3309827ec81e6a385b0dd92d68ab6c6d0a5
avellcaro <cg2000avellaneda@gmail.com>
2026-06-11 16:15:16 -0300

    Punto 5: agregar detalle desde Favoritos y mantener favoritos/carrito en cards

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -27,17 +27,21 @@ const Favorite = () => {
       <div className="favoritos-lista">
         {favoriteItems.map((product) => (
           <div key={product.favoritoId ?? product.id} className="favorito-item">
-            <img
-              src={getProductImageSrc(product)}
-              alt={product.nombre}
-              className="favorito-imagen"
-            />
+            <Link to={`/products/${product.id}`} className="favorito-imagen-link">
+              <img
+                src={getProductImageSrc(product)}
+                alt={product.nombre}
+                className="favorito-imagen"
+              />
+            </Link>
 
             <div className="favorito-info">
-              <p className="favorito-nombre">{product.nombre}</p>
-              <p className="favorito-precio">
-                ${Number(product.precio).toLocaleString('es-AR')}
-              </p>
+              <Link to={`/products/${product.id}`} className="favorito-detalle-link">
+                <p className="favorito-nombre">{product.nombre}</p>
+                <p className="favorito-precio">
+                  ${Number(product.precio).toLocaleString('es-AR')}
+                </p>
+              </Link>
             </div>
 
             <div className="favorito-acciones">
@@ -45,6 +49,7 @@ const Favorite = () => {
                 Ver detalle
               </Link>
               <button
+                type="button"
                 className="btn-quitar-favorito"
                 onClick={() => removeFromFavorite(product.id)}
               >
